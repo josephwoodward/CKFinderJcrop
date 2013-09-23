5f63a6343c2781a17266f713ac0c15769b90e86e
PlamenVasilev <plamen.vasilev.vasilev@gmail.com>
2013-09-23 19:14:33 +0300

    Fix filename upload

--- lib/Boot.php
+++ lib/Boot.php
@@ -97,10 +97,12 @@ class Image
         $dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
 
         imagecopyresampled( $dst_r, $img_r, 0, 0, $_POST['x'], $_POST['y'], $targ_w, $targ_h, $_POST['w'], $_POST['h'] );
-
-        $filename = $this->baseDir . '/uploads/images/' . $this->folderName . '/' . $this->createFilename($this->fileName);
+		
+		$saveDir = dirname($this->baseDir . $this->imageUrl);
+		$saveFile = $this->createFilename($this->fileName);
+        $filename = $saveDir. '/' . $saveFile;
         imagejpeg($dst_r, $filename, $jpeg_quality);
-
+		return $saveFile;
     }
 
     private function createFilename($fileName)
